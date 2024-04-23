dde8413bdcd764f7065f4ea9e62a964561b61e19
orlando manfredini <orlando.manfre1@gmail.com>
2024-04-23 11:20:25 +0200

    ADD: component section video

--- src/components/AppSectionVideo.vue
+++ src/components/AppSectionVideo.vue
@@ -3,6 +3,9 @@
         <div class="box">
             <img :src="store.sectionVideo.src" alt="">
             <h2>{{ store.sectionVideo.title }}</h2>
+            <div class="icon-video">
+                <font-awesome-icon :icon="['fas', 'play']" />
+            </div>
         </div>
     </section>
 </template>
@@ -34,11 +37,29 @@ import { store } from '../store';
 
     h2{
         position: absolute;
-        top: 60%;
+        top: 65%;
         left: 50%;
         transform: translate(-50%, -50%);
         font-size: 40px;
         color: white;
+    }
+
+    .icon-video{
+        color: rgb(242, 134, 10);
+        padding: 30px;
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        font-size: 22px;
+        position: absolute;
+        top: 45%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        border: 2px solid rgb(242, 134, 10);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        
     }
 }
 
--- src/main.js
+++ src/main.js
@@ -8,9 +8,9 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faPlay } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faMagnifyingGlass)
+library.add(faMagnifyingGlass, faPlay)
 
 const app = createApp(App)
 app.component('font-awesome-icon', FontAwesomeIcon)
